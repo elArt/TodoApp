5638cc00b1e6fc7f00311ab3bacead817ef20692
elArt <temka77777@gmail.com>
2020-01-22 17:03:09 +0300

    feature/todo-list, add edit button

--- src/App.js
+++ src/App.js
@@ -1,53 +1,100 @@
-import React, { Component } from 'react';
-import Todo from './component/Todo/';
-import InputField from './component/InputField/';
-import { Typography } from 'antd';
-import styles from './styles.module.scss';
-import * as data from './mocks/todo.json'
+import React, { Component } from "react";
+import Todo from "./component/Todo/";
+import InputField from "./component/InputField/";
+import { Typography } from "antd";
+import styles from "./styles.module.scss";
+import * as data from "./mocks/todo.json";
+import ModalWindow from "./component/ModalWindow";
 
 const { Title } = Typography;
 const todoListFromJson = data.items;
 
 class App extends Component {
   state = {
-      todoList: ''
-  }
+    todoList: "",
+    visible: false,
+    editText: "",
+    id: ""
+  };
   deletPost = (id, todoList) => {
-    const newTodo = todoList.filter(todo => todo.id !== id)
+    const newTodo = todoList.filter(todo => todo.id !== id);
     this.setState({
       todoList: newTodo
-    })
-  }
-  editPost = (id, todoList) =>{
-    console.log(id, todoList);
-    
-  }
+    });
+  };
+
+  editPost = (id, text) => {
+    this.setState({
+      visible: true,
+      editText: text,
+      id: id
+    });
+  };
+
+  handleOk = (editId, todoList, editText) => {
+    const newTodo = todoList.filter(todo =>
+      todo.id === editId ? (todo.text = editText) : todo
+    );
+    this.setState({
+      visible: false,
+      todoList: newTodo
+    });
+  };
+
+  handleCancel = () => {
+    this.setState({
+      visible: false
+    });
+  };
+
+  changeModalValue = e => {
+    this.setState({
+      editText: e.target.value
+    });
+  };
+
   addTodoList = (inputValue, todos) => {
     const newTodo = {
       id: Date.now(),
       text: inputValue
-    }
+    };
     this.setState({
       todoList: [...todos, newTodo]
-    })
-  }
-  componentDidMount(){
+    });
+  };
+
+  componentDidMount() {
     this.setState({
       todoList: todoListFromJson
-    })
+    });
   }
 
   render() {
-    const todoList =  this.state.todoList
+    const todoList = this.state.todoList;
+    const visible = this.state.visible;
+    const editText = this.state.editText;
+    const editId = this.state.id;
     return (
       <div>
         <Title className={styles.title}>TODO</Title>
         <InputField todos={todoList} addTodoList={this.addTodoList} />
-        <Todo todos={todoList} deletTodoTask={this.deletPost} editPost={this.editPost} />
+        <Todo
+          todos={todoList}
+          deletTodoTask={this.deletPost}
+          editPost={this.editPost}
+        />
+        <ModalWindow
+          handleOk={this.handleOk}
+          handleCancel={this.handleCancel}
+          changeModalValue={this.changeModalValue}
+          editId={editId}
+          todoList={todoList}
+          editText={editText}
+          visible={visible}
+        ></ModalWindow>
       </div>
-    )
+    );
   }
-
 }
 
 export default App;
--- src/component/Todo/index.js
+++ src/component/Todo/index.js
@@ -5,37 +5,34 @@ import styles from "./styles.module.scss";
 const Todo = ({ todos, deletTodoTask, editPost }) => {
   const todoList = todos.length
     ? todos.map(todo => (
-        <div key={todo.id} >
+        <div key={todo.id}>
           {todo.text}
           <span
-          className = {styles.iconsEdit}
-          onClick = {()=>{
-            editPost(todo.id, todo.text, todos)
-          }}>
+            className={styles.iconsEdit}
+            onClick={() => {
+              editPost(todo.id, todo.text, todos);
+            }}
+          >
             <Icon type="edit" />
           </span>
           <span
-          className = {styles.iconsDelet}
+            className={styles.iconsDelet}
             onClick={() => {
               deletTodoTask(todo.id, todos);
-            }}>
+            }}
+          >
             {" "}
-            <Icon type="delete"/>
+            <Icon type="delete" />
           </span>
         </div>
       ))
     : "You need more learn REACT";
 
-    
   return (
-      <List
+    <List
       bordered
       dataSource={todoList}
-      renderItem={item => (
-        <List.Item>
-          {item}
-        </List.Item>
-      )}
+      renderItem={item => <List.Item>{item}</List.Item>}
     />
   );
 };
